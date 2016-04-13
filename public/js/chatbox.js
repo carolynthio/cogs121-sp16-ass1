fc86042e41668054d67f97b1323ee23aaa8b91c9
Alexandra Huang <alexandra.huang@gmail.com>
2016-04-12 18:14:42 -0700

    Removed the quotes from displayed messages and moved the quotes around the example at top of chat page

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -42,7 +42,7 @@
           '</div>' +
           '</div>' +
           '<div class="message-content">' +
-          parsedData.message +
+          JSON.parse(parsedData.message) +
           '</div>';
       return result;
     }
